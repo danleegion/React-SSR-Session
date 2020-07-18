8a604f0fbdb228728da07d6f6ee4b01ef98b7915
Daniel Lee Cher Chen <daniel_cc_lee@manulife.ca>
2020-07-18 18:28:55 -0400

    Server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
-import express from "express";
-import compression from "compression";
-import ssr from "./routes/ssr";
+import express from 'express';
+import compression from 'compression';
+import ssr from './routes/ssr';
+import { redisSession } from './configuration/session/redis';
+import https from 'https';
+import fs from 'fs';
+
 const app = express();
 
-app.use(compression());
-app.use(express.static("public"));
+//This is optional and is used to trust first level proxy
+app.set('trust proxy', 1);
 
-app.use("/firstssr", ssr);
+app.use(compression());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(redisSession);
+
+app.use('/firstssr', ssr);
 
 const port = process.env.PORT || 3030;
-app.listen(port, function listenHandler() {
-  console.info(`Running on ${port}...`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+    },
+    app
+  )
+  .listen(port, () => {
+    console.info(`Running on HTTPS Port ${port}`);
+  });
